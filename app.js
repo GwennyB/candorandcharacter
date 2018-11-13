844931b20a91aafcd813cf0a9faf36e1be238a04
jason few <omence11@gmail.com>
2018-11-12 17:30:37 -0800

    quiz function started

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-'use strict';
+'use strict'
+
+//build constructor for quiz objects #20
+function Quiz([], [], startButtonID, divID, modalID) {
+  this.questions = [];
+  this.results = [];
+  this.startButtonID = startButtonID;
+  this.divID = divID;
+  this.modalID = modalID;
+}
+
+Quiz.prototype.renderToken = function() {
+  var startB = document.getElementById(this.startButtonID);
+  startB.addEventListener('click', function renderQuiz() {
+    var modal = document.getElementById(this.modalID);
+  
+
+  });
+
+};
+
+Quiz.prototype.renderBlur = function() {
+  document.getdivById(this.divID);
+
+};
+
+
+Quiz.prototype.handleToken = function() {
+
+};
+
+Quiz.prototype.handleSubmi = function () {
+
+};
