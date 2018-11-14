5ca00fe348c8047dc2e6cb1007eaa88d4ceae1b6
jason few <omence11@gmail.com>
2018-11-14 09:44:56 -0800

    pushtry2

--- quiz.js
+++ quiz.js
@@ -1,64 +1,82 @@
 'use strict';
-var newCharacter = new Character();
-//build quiz1 function #20
-function Quiz(name, [], buttonID, formID) {
+//var newCharacter = new Character();
 
-this.name = name
-this.questions = [];
-this.buttonID = buttonID;
-this.forID = formID;
-this.results = [];
+var quizOne = [];
+var quizTwo = [];
+var quizThree = [];
 
+function quizOneResults() {
+  var quizResults = document.getElementById('quiz-one');
+  quizResults.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-Quiz.prototype.buttonAndQuiz = function() {
-    var container = document.getElementById('buttonContainer');
-    var startb = document.createElement('button');
-    startb.setAttribute('type', 'submit');
-    startb.id = buttonID;
-    container.appendChild('startb')
-    startb.addEventListener('click', function renderQuiz() {
-      var quizPrompt = document.getElementById(this.formID).style.display = 'block';
+    var quesOne = event.target.question1.value;
+    quizOne.push(quesOne);
 
-      var qOne = document.getElementById('qu1');
-      qOne.textContent = this.questions[0];
+    var quesTwo = event.target.question2.value;
+    if (quesTwo <= 2) quizOne.push('Half Orc');
+    if (quesTwo > 2 && quesTwo <= 4) quizOne.push('Human');
+    if (quesTwo > 4 && quesTwo <= 6) quizOne.push('Dwarf');
+    if (quesTwo > 6 && quesTwo <= 8) quizOne.push('Elf');
+    if (quesTwo > 8 && quesTwo <= 10) quizOne.push('Gnome');
 
-      var qTwo = document.getElementById('qu2');
-      qTwo.textContent = this.questions[1];
+    var quesThree = event.target.question3.value;
+    if (quesThree > 1) quizOne.push('Evil');
+    else quizOne.push('Good');
 
-      var qThree = document.getElementById('qu3');
-      qThree.textContent = this.questions[2];
-
-      var qFour = document.getElementById('qu4');
-      qFour.textContent = this.questions[3];
-
-      var qFive = document.getElementById('qu5');
-      qFive.textContent = this.questions[4];
-
-      var qSix = document.getElementById('qu6');
-      qSix.textContent = this.questions[5];
-
-      container.removechild(startb);
-    }
+    var quesFour = event.target.question4.value;
+    quizOne.push(quesFour);
+  });
 }
+quizOneResults();
 
-Quiz.prototype.getResults = function() {
-    var quizResults = document.getElementById(this.formID)
-    quizResults.addEventListener('submit', function(event) {
-        var quesOne = event.target.Question1.value;
-        var quesTwo = event.target.Question2.value;
-        var quesThree = event.target.Question3.value;
-        var quesFour = event.target.Question3.value;
-        var quesFive = event.target.Question3.value;
-        var quesSix = event.target.Question3.value;
-       
-        this.results.push(quesOne);
-        this.results.push(quesTwo);
-        this.results.push(quesThree);
-        this.results.push(quesFour);
-        this.results.push(quesFive);
-        this.results.push(quesSix);
+function quizTwoResults() {
+  var quizResults = document.getElementById('quiz-two');
+  quizResults.addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    var quesOne = event.target.question1.value;
+    quizTwo.push(quesOne);
 
-        document.getElementById("formID").style.display = "none";
-    }
+    var quesTwo = event.target.question2.value;
+    quizTwo.push(quesTwo);
+
+    var quesThree = event.target.question3.value;
+    quizTwo.push(quesThree);
+
+    var quesFour = event.target.question4.value;
+    quizTwo.push(quesFour);
+
+    var quesFive = event.target.question5.value;
+    quizTwo.push(quesFive);
+
+    var quesSix = event.target.question6.value;
+    quizTwo.push(quesSix);
+  });
 }
+quizTwoResults();
+
+function quizThreeResults() {
+  var quizResults = document.getElementById('quiz-three');
+  quizResults.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var quesOne = event.target.question1.value;
+    quizThree.push(quesOne);
+
+    var quesTwo = event.target.question2.value;
+    quizThree.push(quesTwo);
+
+    var quesThree = event.target.question3.value;
+    quizThree.push(quesThree);
+
+    var quesFour = event.target.question4.value;
+    quizThree.push(quesFour);
+  });
+}
+quizThreeResults();
+
+console.log(quizOne);
+console.log(quizTwo);
+console.log(quizThree);
+//ff
