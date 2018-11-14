ce22f119973043dcb989705534e6f7de07b0cb8c
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-11-14 13:44:40 -0800

    new character functions working

--- app.js
+++ app.js
@@ -44,3 +44,69 @@ Character.prototype.renderCharacter = function() { // this will come from query 
 };
 
 
+
+// quiz object constructor
+function Quiz (quizNum) {
+  this.quizNum = quizNum;
+  this.questions = [];
+  this.token;
+  this.blurbox;
+  this.quizForm;
+  this.outputs = [];
+}
+
+// quiz prototype functions:
+// create, place, and activate token
+Quiz.prototype.makeToken = function() {
+  this.blurbox.style.zIndex = '75';
+  this.token.style.zIndex = '100';
+  this.quizForm.style.display = 'none';
+  this.token.addEventListener('click',this.handleToken);
+};
+
+Quiz.prototype.handleToken = function(event) {
+  event.preventDefault();
+  var getQuiz = quizzes.find(obj => {return obj.token.alt === this.alt});
+  getQuiz.renderQuiz();
+};
+
+// token click handler: render quiz
+Quiz.prototype.renderQuiz = function() {
+  // change display on quiz form and token
+  this.token.style.zIndex = '-100';
+  this.quizForm.style.display = 'block';
+  this.quizForm.style.zIndex = '100';
+  this.quizForm.addEventListener('submit', this.handleSubmit);
+};
+
+// quiz submit handler: process inputs into outputs, push to character
+Quiz.prototype.handleSubmit = function(event) {
+  event.preventDefault();
+  var getQuiz = quizzes.find(obj => {return obj.quizForm.textContent === this.textContent});
+  // push quiz results to this.outputs
+  getQuiz.outputs = ['output','output','output','output'];
+  // call 'flipVisibility'
+  getQuiz.flipVisibility();
+};
+
+// hide token, quiz, and blur box
+Quiz.prototype.flipVisibility = function() {
+  // change display on quiz form, blur box
+  this.blurbox.style.zIndex = '-100';
+  this.quizForm.style.zIndex = '-100';
+  this.renderCharacter();
+};
+
+Quiz.prototype.renderCharacter = function() { // this will come from query string ('existingcharacter.js') or 'new character' ('newcharacter.js')
+  // render quiz outputs
+  for (var outputs in this.outputs) {
+    this.blurbox.childNodes[1+2*outputs].textContent = this.outputs;
+  }
+
+  // move to next quiz
+  if (this.quizNum < 2) {
+    quizzes[this.quizNum+1].makeToken();
+  } else {
+    alert('Congratulations... you made a character!');
+  }
+};
--- newcharacter.js
+++ newcharacter.js
@@ -1,63 +1,6 @@
 'use strict';
 
-// FUNCTIONS
-
-// quiz object constructor
-function Quiz (quizNum) {
-  this.quizNum;
-  this.questions = [];
-  this.token = {};
-  this.blurbox = {};
-  this.quizForm = {};
-  this.outputs = [];
-}
-
-// quiz prototype functions:
-// create, place, and activate token
-Quiz.prototype.makeToken = function() {
-  this.blurbox.style.zIndex = '75';
-  this.token.style.zIndex = '100';
-  this.token.addEventListener('click',this.renderQuiz);
-};
-
-// token click handler: render quiz
-Quiz.prototype.renderQuiz = function(event) {
-  event.preventDefault();
-  // change display on quiz form and token
-  this.token.style.display = '-100';
-  this.quizForm.style.display = '100';
-};
-
-// quiz submit handler: process inputs into outputs, push to character
-Quiz.prototype.handleSubmit = function(event) {
-  // push quiz results to this.outputs
-  this.outputs = ['output','output','output','output'];
-  // call 'flipVisibility'
-  this.flipVisibility();
-};
-
-// hide token, quiz, and blur box
-Quiz.prototype.flipVisibility = function() {
-  // change display on quiz form, blur box
-  this.blurbox.style.zIndex = '-100';
-  this.quizForm.style.display = '-100';
-  // call allCharacters[4].renderCharacter
-};
-
-Quiz.prototype.renderCharacter = function() { // this will come from query string ('existingcharacter.js') or 'new character' ('newcharacter.js')
-  // render quiz outputs
-  for (var outputs in this.outputs) {
-    this.blurbox.childNodes[1+2*outputs].textContent = this.outputs;
-  }
-
-  // move to next quiz
-  if (this.quizNum < 4) {
-    quizzes[this.quizNum+1].makeToken();
-  } else {
-    alert('Congratulations... you made a character!');
-  }
-};
-  
+// see app.js for common functions
 
 // PAGE EXECUTION
 // page must load with all blur divs forward, all quizzes back, quiz tokens back
@@ -77,12 +20,12 @@ var quizToken = [9,13,9]; // this is the childNode number for the token
 var quizForm = [11,15,11]; // this is the childNode number for the form
 
 // create quiz objects
-for (var quizNum = 0; Num < 3; Num++) {
-  quizzes.push(new Quiz(quizNum));
-  quizzes[quizNum].questions.push(quizQuestions[quizNum]);
-  quizzes[quizNum].blurbox.push(document.getElementById(quizBlurbox[quizNum]));
-  quizzes[quizNum].token.push(quizzes[quizNum].blurbox.childNodes[quizToken]);
-  quizzes[quizNum].quizForm.push(quizzes[quizNum].blurbox.childNodes[quizForm]);
+for (var newQuiz = 0; newQuiz < 3; newQuiz++) {
+  quizzes.push(new Quiz(newQuiz));
+  quizzes[newQuiz].questions.push(quizQuestions[newQuiz]);
+  quizzes[newQuiz].blurbox = document.getElementById(quizBlurbox[newQuiz]);
+  quizzes[newQuiz].token = quizzes[newQuiz].blurbox.childNodes[quizToken[newQuiz]];
+  quizzes[newQuiz].quizForm = quizzes[newQuiz].blurbox.childNodes[quizForm[newQuiz]];
 }
 
 // PROTO: show and activate listener on 1st quiz
