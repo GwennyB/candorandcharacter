4ff96a400f782353ce46bdd41ab215130a9baeff
Andrew Hinojosa <drewsview34@gmail.com>
2018-11-13 16:13:27 -0800

    makin it pretty. Gurus turn

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var allCharacters = [];
+
+
 // TASK CARD: build Character object constructor function #18
 function Character() {
   this.quizOne = []; // player name, character name, race, alignment (receive from quiz 1)
   this.quizTwo = []; // strength, dexterity, constitution, intelligence, wisdom, charisma (receive from quiz 2)
   this.quizThree = []; // armor class, initiative, speed, hit points (receive from quiz 3)
+  this.renderURL = '';
 
   if (allCharacters.length>3) {
     allCharacters = allCharacters.splice(0,1).push(this);
@@ -17,12 +20,12 @@ function Character() {
 // TASK CARD: build Character object constructor function #18
 Character.prototype.sendToLocalStorage = function() {
   var charJSON = JSON.stringify(allCharacters); // encode updated characters list
-  localStorage.setItem('allCharacters',charJSON); // send updated characters list to LS
+  localStorage.setItem('storedCharacters',charJSON); // send updated characters list to LS
 };
 
 // TASK CARD: build Character object constructor function #18
 Character.prototype.getFromLocalStorage = function() {
-  var storedCharacters = localStorage.getItem('storedCharacters') || ['seeTaskCard17']; // get array of 4 chars from LS, or use hard-coded chars
+  var storedCharacters = localStorage.getItem('storedCharacters'); // || ['seeTaskCard17']; // get array of 4 chars from LS, or use hard-coded chars
   return JSON.parse(storedCharacters);
 };
 
@@ -30,6 +33,21 @@ Character.prototype.getFromLocalStorage = function() {
 // load page 'existingcharacter.html', which launches 'existingcharacter.js' and renders with this function
 Character.prototype.renderCharacter = function() { // this will come from query string ('existingcharacter.js') or 'new character' ('newcharacter.js')
   // render this.quizOne[i]
+  var quizOneAnswers = document.getElementById('quizone');
+  for (var outputOne in this.quizOne) {
+    quizOneAnswers.childNodes[1+2*outputOne].textContent = this.quizOne[outputOne];
+  }
   // render this.quizTwo[i]
+  var quizTwoAnswers = document.getElementById('quiztwo');
+  for (var outputTwo in this.quizTwo) {
+    quizTwoAnswers.childNodes[1+2*outputTwo].textContent = this.quizTwo[outputTwo];
+  }
   // render this.quizThree[i]
+  var quizThreeAnswers = document.getElementById('quizthree');
+  for (var outputThree in this.quizThree) {
+    quizThreeAnswers.childNodes[1+2*outputThree].textContent = this.quizThree[outputThree];
+  }
 };
+
+
+console.log('CL3',document.getElementById('characterlist'));
